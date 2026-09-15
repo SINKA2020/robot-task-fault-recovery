abb82431f4c6d16830146b5d726f1103b13cf96a
SINKA2020 <1047014641@qq.com>
2026-09-15 22:17:50 +0800

    Remove incorrectly uploaded files

--- __init__ (7).py
+++
@@ -1,1 +0,0 @@
-"""Scene graph modules."""
--- __init__.py
+++
@@ -1,5 +0,0 @@
-"""Scene graph based robot fault detection and recovery."""
-from pkgutil import extend_path
-
-# Allow catkin-generated scene_graph_system.msg to share this package.
-__path__ = extend_path(__path__, __name__)
